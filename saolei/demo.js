d69b1cf501998b27a87ce04e3d0b0e4c628872c5
will <will3virgo@163.com>
2018-05-24 17:26:28 +0800

    17:26

---
+++ saolei/demo.js
@@ -0,0 +1,117 @@
+var oBtn = document.getElementsByClassName('start-game')[0],
+    oWrapper = document.getElementsByClassName('wrapper')[0],
+    oLeiBox = document.getElementsByClassName('lei-box')[0],
+    oResidueBoom = document.getElementsByClassName('residue')[0],
+    oClose = document.getElementsByClassName('close')[0],
+    aBoom = document.getElementsByClassName('boom');
+
+bindEvent();
+function bindEvent() {
+    oBtn.onclick = function () {
+        this.style.display = 'none';
+        oWrapper.style.display = 'block';
+        document.body.style.backgroundColor = '#ccc';
+        init();
+    };
+    oClose.onclick = function () {
+        oLeiBox.innerHTML = '';
+        oWrapper.style.display = 'none';
+        oBtn.style.display = 'block';
+        document.body.style.backgroundColor = '#fff';
+    };
+    oLeiBox.oncontextmenu = function () {
+        return false;
+    };
+    oLeiBox.onmousedown = function (e) {
+        var event = e || window.event,
+            target = event.target || event.srcElement;
+
+        if (e.button === 0) {
+            leftBtnClick(target);
+        } else if (e.button === 2) {
+            console.log('right');
+        }
+    };
+}
+
+function leftBtnClick(ele) {
+    var _ele = ele,
+        len = aBoom.length,
+        aroundBoom = 0,
+        arr = _ele && _ele.getAttribute('id').split('-'),
+        x = arr && +arr[0],
+        y = arr && +arr[1];
+
+    _ele.style.backgroundColor = '#FFF';
+
+    if (_ele && _ele.classList.contains('boom')) {
+        for (var i = 0; i < len; i++) {
+            aBoom[i].innerHTML = 'Boom';
+        }
+        window.setTimeout(function () {
+            var restart = window.confirm('菜，还来不?');
+            if (restart) {
+                oLeiBox.innerHTML = '';
+                init();
+            } else {
+                oLeiBox.innerHTML = '';
+                oWrapper.style.display = 'none';
+                oBtn.style.display = 'block';
+                document.body.style.backgroundColor = '#fff';
+            }
+        }, 800);
+    } else {
+        _ele.classList.add('tips');
+        for (var r = x - 1; r <= x + 1; r++) {
+            for (var l = y - 1; l <= y + 1; l++) {
+                var aroundEle = document.getElementById(r + '-' + l);
+                if (aroundEle && aroundEle.classList.contains('boom')) {
+                    aroundBoom++;
+                }
+            }
+        }
+        if (aroundBoom === 0) {
+            aroundBoom = '';
+        }else {
+            aroundBoom += '';
+        }
+        _ele.innerHTML = aroundBoom;
+        if (aroundBoom === '') {
+            for (var _r = x - 1; _r <= x + 1; _r++) {
+                for (var _l = y - 1; _l <= y + 1; _l++) {
+                    var temp = document.getElementById(_r + '-' + _l);
+                    if ( temp && !temp.classList.contains('boom')) {
+                        if (!temp.classList.contains('done')) {
+                            temp.classList.add('done');
+                            leftBtnClick(temp);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function init() {
+    var oFraf = document.createDocumentFragment(),
+        boomNum = 10;
+
+    for (var row = 0; row < 10; row++) {
+        for (var line = 0; line < 10; line++) {
+            var _block = document.createElement('div');
+            _block.style.backgroundColor = '#888';
+            _block.classList.add('block');
+            _block.setAttribute('id', row + '-' + line);
+            oFraf.appendChild(_block);
+        }
+    }
+    oLeiBox.appendChild(oFraf);
+
+    while (boomNum) {
+        var boom = Math.floor(Math.random() * 100);
+        if (!oLeiBox.children[boom].classList.contains('boom')) {
+            oLeiBox.children[boom].classList.add('boom');
+            boomNum--;
+        }
+    }
+}
